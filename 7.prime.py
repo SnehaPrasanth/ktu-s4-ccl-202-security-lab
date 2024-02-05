bef24aaeeef009175c9b431dd148fe20b1e56045
Sneha Prasanth <117026646+SnehaPrasanth@users.noreply.github.com>
2024-02-05 23:05:54 +0530

    7.prime.py

--- 7.prime.py
+++ 7.prime.py
@@ -1,10 +1,16 @@
 n = int(input("Enter a number: "))
+
 if n > 1:
+    prime_flag = True
     for i in range(2, int(n/2) + 1):
         if (n % i) == 0:
-            print("Not prime")
+            prime_flag = False
             break
-    else:
+    if prime_flag:
         print("Prime")
+    else:
+        print("Not prime")
+elif n == 1 or n == 0:
+    print("Neither prime nor composite")
 else:
-    print("Not prime")
+    print("Enter a number greater than 1")
